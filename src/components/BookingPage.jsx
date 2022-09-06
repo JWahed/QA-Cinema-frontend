ae3bd12dfe7621cacc62300a85940b4a8cfef5eb
Khaleeq <khaleeq.tanweer@gmail.com>
2022-09-06 15:15:09 +0100

    bootstrapped + completed booking form

--- src/components/BookingPage.jsx
+++ src/components/BookingPage.jsx
@@ -3,24 +3,22 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const BookingPage = () => {
+const BookingPageRefactor = () => {
   const [availableMovies, setAvailableMovies] = useState([]);
-  const [title, setSelectedMovie] = useState('');
-  const [date, setSelectedDate] = useState('');
-  const [time, setSelectedTime] = useState('');
-  const [seats, setSelectedSeat] = useState('');
-  const [ticketType, setTicketType] = useState('');
-  const [name, setName] = useState('');
+  const [title, setSelectedMovie] = useState("");
+  const [date, setSelectedDate] = useState("");
+  const [time, setSelectedTime] = useState("");
+  const [seats, setSelectedSeat] = useState("");
+  const [ticketType, setTicketType] = useState("");
+  const [name, setName] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const ticketPrice = [10.75,5.65, 7.25 ]
-
-
+  const ticketPrice = [10.75, 5.65, 7.25];
 
   const onSubmitBooking = async (e) => {
     e.preventDefault();
     setIsLoading(true);
 
-    const res = await axios.post('/api/booking', {
+    const res = await axios.post("/api/booking", {
       fullTitle: title,
       date: date,
       movieTime: time,
@@ -30,14 +28,11 @@ const BookingPage = () => {
     });
     setIsLoading(false);
     console.log(res.data);
-
-    
   };
-
 
   useEffect(() => {
     axios
-      .get('http://localhost:5000/api/movies')
+      .get("http://localhost:5000/api/movies")
       .then((res) => {
         setAvailableMovies(res.data);
       })
@@ -45,33 +40,47 @@ const BookingPage = () => {
   }, []);
 
   return (
-    <div className='BookingForm'>
-      <h3> Book Tickets </h3>
+    <div className="BookingForm">
+      <br/>
+      <h4> Book Tickets Here</h4>
+      <br />
       <form
-        className='Form'
+        className="Form"
         onSubmit={(e) => {
           onSubmitBooking(e);
         }}
       >
-        <label htmlFor='name'>Movie</label>
-        <select required onChange={(e) => setSelectedMovie(e.target.value)}>
-          <option value=''>Select a Movie</option>
-          {availableMovies.map(({fullTitle }) => (
+        <div className='body'>
+        <label for="movieTitle" class="form-label">
+          Movie:
+        </label>
+        <select
+          class="form-select"
+          aria-label="Default select example"
+          required
+          onChange={(e) => setSelectedMovie(e.target.value)}
+        >
+          <option selected>Select a Movie</option>
+          {availableMovies.map(({ title }) => (
             <option
-              value={fullTitle}
+              value={title}
               onChange={(e) => setSelectedMovie(e.target.value)}
             >
-              {fullTitle}
+              {title}
             </option>
           ))}
         </select>
         <br />
-        <label htmlFor='movieTitle'>Date</label>
-        <select required onChange={(e) => setSelectedDate(e.target.value)}>
-          <option value=''>-Select a Date-</option>
-          <option onChange={(e) => setSelectedDate(e.target.value)}>
-            Friday: 09/09/2022
-          </option>
+        <label for="movieTitle" class="form-label">
+          Date:
+        </label>
+        <select
+          class="form-select"
+          aria-label="Default select example"
+          required
+          onChange={(e) => setSelectedDate(e.target.value)}
+        >
+          <option value="">Select a Date</option>
           <option onChange={(e) => setSelectedDate(e.target.value)}>
             Saturday: 10/09/2022
           </option>
@@ -81,37 +90,61 @@ const BookingPage = () => {
           <option onChange={(e) => setSelectedDate(e.target.value)}>
             Monday: 12/09/2022
           </option>
+          <option onChange={(e) => setSelectedDate(e.target.value)}>
+            Tuesday: 13/09/2022
+          </option>
+          <option onChange={(e) => setSelectedDate(e.target.value)}>
+            Wednesday: 14/09/2022
+          </option>
+          <option onChange={(e) => setSelectedDate(e.target.value)}>
+            Thursday: 15/09/2022
+          </option>
+          <option onChange={(e) => setSelectedDate(e.target.value)}>
+            Friday: 16/09/2022
+          </option>
           ))
         </select>
         <br />
-        <br />
-        <label htmlFor='movieTitle'>Time</label>
-        <select required onChange={(e) => setSelectedTime(e.target.value)}>
-          <option value=''>-Select a Time-</option>
-          <option>10:30 Screen 1</option>
-          <option>11:30 Screen 2</option>
-          <option>16:00 Screen 3</option>
-          <option>20:00 Screen 5</option>
-          ))
-        </select>
-        <br />
-        <br />
-      <Link to={{ pathname: `/screens` }} target="_blank">
-        <button id="screenButton" type="button">
-          Please click for screen info{" "}
-        </button>
-      </Link>
-      <br />
-        <label htmlFor='seats'>No. of Seats:</label>
-        <input
+        <label for="movieTitle" class="form-label">
+          Time:
+        </label>
+        <select
+          class="form-select"
+          aria-label="Default select example"
           required
-          type='number'
-          placeholder='Enter no. of seats...'
-          id='seats'
+          onChange={(e) => setSelectedTime(e.target.value)}
+        >
+          <option value="">Select a Time</option>
+          <option>10:00 Screen 1 (Deluxe)</option>
+          <option>12:30 Screen 2 (Standard)</option>
+          <option>16:00 Screen 3 (Standard)</option>
+          <option>20:00 Screen 4 (Deluxe)</option>
+          ))
+        </select>
+        <br />
+        <Link to={{ pathname: `/screens` }} target="_blank">
+          <button type="button" class="btn btn-info screenBtn">
+            Please click for screen info{" "}
+          </button>
+        </Link>
+        <br />
+        <br />
+        <label for="movieTitle" class="form-label">
+          Seats:
+        </label>
+        <input
+          aria-describedby="inputGroup-sizing-default"
+          class="form-control"
+          aria-label="Sizing example input"
+          required
+          type="number"
+          placeholder="Enter number of seats"
+          id="seats"
           onChange={(e) => setSelectedSeat(e.target.value)}
         />
         <br />
-        <p>Please select your ticket type:</p>
+        <label for="movieTitle" class="form-label"> Please select your ticket type:</label>
+        <br />
         <input
           required
           type='radio'
@@ -119,7 +152,7 @@ const BookingPage = () => {
           name='ticketType'
           onChange={(e) => setTicketType(e.target.value)}
         />
-        <label htmlFor='adult'>Adult Seats (£{ticketPrice[0]} each)</label>
+        <label htmlFor='adult' class="form-label"> Adult Seats (£{ticketPrice[0]} each)</label>
         <br />
         <input
           required
@@ -128,7 +161,7 @@ const BookingPage = () => {
           name='ticketType'
           onChange={(e) => setTicketType(e.target.value)}
         />
-        <label htmlFor='child'>Child Seats (£{ticketPrice[1]} each)</label>
+        <label htmlFor='child' class="form-label"> Child Seats (£{ticketPrice[1]} each)</label>
         <br />
         <input
           required
@@ -137,24 +170,29 @@ const BookingPage = () => {
           name='ticketType'
           onChange={(e) => setTicketType(e.target.value)}
         />
-        <label htmlFor='concession'>Concession Seats (£{ticketPrice[2]} each)</label>
+        <label htmlFor='concession' class="form-label"> Concession Seats (£{ticketPrice[2]} each)</label>
         <br />
-        <label htmlFor='name'>Booker name:</label>
+        <br />
+        <label htmlFor='name' class="form-label">Booker Name:</label>
         <input
           required
           type='text'
+          aria-describedby="inputGroup-sizing-default"
+          class="form-control"
+          aria-label="Sizing example input"
           placeholder='Enter name...'
           id='bookerName'
           onChange={(e) => setName(e.target.value)}
         />
         <br />
-        <button type='submit'>
+        <button type="submit" class="btn btn-primary bookingBtn">
           {isLoading && <i className='fas fa-spinner fa-pulse' />}
           {!isLoading && 'Book Tickets!'}
         </button>
+        </div>
       </form>
     </div>
   );
 };
 
-export default BookingPage;
+export default BookingPageRefactor;
