2bf9579df486ed019ed02edd078ec16a6e679bbb
JWahed <jak.clev@gmail.com>
2022-09-06 23:33:57 +0100

    Update App.js
    Renders "loading..." if authentication keys are falsy otherwise renders actual website

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Homepage from './components/routes/Homepage';
-import NotFound from './components/routes/NotFound';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./components/routes/Homepage";
+import NotFound from "./components/routes/NotFound";
 
+import { Auth0Provider } from '@auth0/auth0-react';
+import PropTypes from 'prop-types';
 
-function App() {
+function App({ authDomainURL, authClientID }) {
+  
+  if (!authDomainURL || !authClientID) {
+    return (
+      <h1 id="load">Loading...</h1>
+    );
+  }
+
   return (
     <div className="App">
+      <Auth0Provider
+          domain={authDomainURL}
+          clientId={authClientID}
+          redirectUri={window.location.origin}
+      >
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Homepage />} />
             <Route path="/*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
+
+      </Auth0Provider>
     </div>
   );
+}
+
+App.propTypes = {
+  authDomainURL: PropTypes.string.isRequired,
+  authClientID: PropTypes.string.isRequired
 }
 
 export default App;
