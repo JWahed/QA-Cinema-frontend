1240287e4c2e2f95dba7a35bc9f7dad95b5a9c0c
Ngou-Tin-Kam <98760980+Ngou-Tin-Kam@users.noreply.github.com>
2022-09-07 00:16:12 +0100

    Added Screens page with content

--- src/pages/Screens.jsx
+++ src/pages/Screens.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React from "react";
+import { Card, CardImg } from "react-bootstrap";
+import standardSeatings from "./images/standard-seatings.jpg";
+import standardSeatingPlan from "./images/standard-seating-plan.jpg";
+import deluxeSeatings from "./images/deluxe-seatings.jpg";
+import deluxeSeatingPlan from "./images/deluxe-seating-plan.jpg";
+
 
 function Screens() {
   return (
-    <div>Screens</div>
-  )
+    <div>
+      <div>
+        <h1>Screens</h1>
+        <p>Here is a preview of the screens with the seats</p>
+      </div>
+
+      <div className="Standard">
+        <div>
+          <Card style={{ width: "30rem" }}>
+            <Card.Body className="body">
+              <Card.Title>Standard Screen</Card.Title>
+              <Card.Img variant="top" className="img" src={standardSeatings} />
+              <Card.Text>Our Standard screen seatings</Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+
+        <div>
+          <Card style={{ width: "30rem" }}>
+            <Card.Body className="body">
+              <Card.Title>Standard Seats</Card.Title>
+              <Card.Img
+                variant="top"
+                className="img"
+                src={standardSeatingPlan}
+              />
+              <Card.Text>Our Standard screen venue</Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <div className="Deluxe">
+        <div>
+          <Card style={{ width: "30rem" }}>
+            <Card.Body className="body">
+              <Card.Title>Deluxe Screen</Card.Title>
+              <Card.Img variant="top" className="img" src={deluxeSeatings} />
+              <Card.Text>Our Dexluxe screen venue</Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+
+        <div>
+          <Card style={{ width: "30rem" }}>
+            <Card.Body className="body">
+              <Card.Title>Deluxe Seats</Card.Title>
+              <Card.Img variant="top" className="img" src={deluxeSeatingPlan} />
+              <Card.Text>Our Standard screen seatings</Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Screens
+export default Screens;
