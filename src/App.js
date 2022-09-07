e2107f9bf20b115df18ff90fbf4b46fab344eeb5
Ngou-Tin-Kam <98760980+Ngou-Tin-Kam@users.noreply.github.com>
2022-09-07 02:51:07 +0100

    Added Content, Accordion and Bootstrap to Classification page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Classification from './pages/Classification';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Classification/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Classification" element={<Classification/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
