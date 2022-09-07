2279bc19adbbf86905a0b7ea0683527276595de3
JWahed <jak.clev@gmail.com>
2022-09-07 13:19:58 +0100

    Update ProfilePage.jsx

--- src/components/routes/ProfilePage.jsx
+++ src/components/routes/ProfilePage.jsx
@@ -1,11 +1,19 @@
 import Header from "../Navigation/Header/Header";
 import Footer from '../Navigation/Footer/Footer';
 import Profile from "../Authentication/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 function ProfilePage() {
+    const { isLoading, isAuthenticated, error } = useAuth0();
+    const navigate = useNavigate();
+
     return (
         <>
             <Header />
+            {error && <p>Authentication Error</p>}
+            {!error && isLoading && <p>Loading...</p>}
+            {!error && !isLoading && !isAuthenticated && navigate("/*")}
             <Profile />
             <Footer />
         </>
