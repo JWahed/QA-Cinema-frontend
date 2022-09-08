e4f0d25ce17ddb21ef6e398bf8b6963ae2e442d9
JWahed <jak.clev@gmail.com>
2022-09-08 14:30:32 +0100

    Update Footer.jsx
    removed inline style

--- src/components/Navigation/Footer/Footer.jsx
+++ src/components/Navigation/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer className="py-3 my-4" style={{}}>
+        <footer className="py-3 my-4">
             <ul className="nav justify-content-center border-bottom pb-3 mb-3">
                 <li className="nav-item">
                     <a href="/" className="nav-link px-2 text-muted">Home</a>
